ae4222ed19f8f5c19b79a4c8868730dd6525d516
Maggie Zhang <maggiezhang@Maggies-MacBook-Pro.local>
2016-12-03 03:14:49 -0500

    navMenu in JQuery
    Naved nav bar to separate js file to reduce redundancy

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("navMenu").innerHTML =
+    '<ul class="nav nav-pills navigation">' +
+        '<li><a href="index.html" target="_top">Sign-Up</a></li>' +
+        '<li role="presentation" class="dropdown active">' +
+            '<a class="dropdown-toggle" data-toggle="dropdown" href="#" target="_top">Menu<b class="caret"></b></a>' +
+            '<ul class="dropdown-menu dropdown-menu-right">' +
+                '<li><a href="index.html" target="_top">Home</a></li>' +
+                '<li><a href="campaign.html" target="_top">The Campaign</a></li>' +
+                '<li><a href="stories.html" target="_top">Stories / News</a></li>' +
+                '<li role="separator" class="divider"></li>' +
+                '<li><a href="action.html" target="_top">Take Action</a></li>' +
+                '<li><a href="pledge.html" target="_top">The Pledge</a></li>' +
+            '</ul>' +
+        '</li>' +
+    '</ul>';
